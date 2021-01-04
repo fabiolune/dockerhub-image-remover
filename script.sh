5c620242b4aa2abece091be6328a0c7dd4e5e506
fabio.lonegro <fabio.lonegro@deltatre.com>
2021-01-05 00:12:52 +0100

    added gitattributes for eol and some comments in script.sh

--- script.sh
+++ script.sh
@@ -5,6 +5,8 @@ pwd=$2
 img=$3
 tag=$4
 
+# this creates a docker hub access token using ROPC oauth flow
 HUB_TOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'$usr'", "password": "'$pwd'"}' https://hub.docker.com/v2/users/login/ | grep -o '"token":[^"]*"[^"]*"' | sed -E 's/".*".*"(.*)"/\1/')
 
+#uses previously obtained jwt access token to invoke the DELETE api and remove tha specific 'image:tag'
 curl -X DELETE -s -H "Accept: application/json" -H "Authorization: JWT $HUB_TOKEN" https://hub.docker.com/v2/repositories/$usr/$img/tags/$tag/
